77b89a4e55dbc9111dcd8ca952315036ebace399
Dima Fitiskin <dfitiskin@gmail.com>
2017-08-11 12:32:54 +0400

    Таймер, звуковое уведомление и улучшенная настройка проверки обновления

--- reloader/reloader.js
+++ reloader/reloader.js
@@ -1,8 +1,54 @@
 'use strict';
 
 const CONTROL_FIELD_ID = 'checksum';
-const TARGET_URL = './check.php';
-const CHECK_PERIOD = 5 * 1000;
+const TIME_FIELD_ID = 'heatStartTime';
+const TIME_OUTPUT = 'heatTime';
+const NOTIFICATION_SOUND = './assets/trembling.mp3';
+
+if (!String.prototype.padStart) {
+  String.prototype.padStart = function padStart(targetLength,padString) {
+    targetLength = targetLength>>0;
+    padString = String(padString || ' ');
+    if (this.length > targetLength) {
+      return String(this);
+    }
+    else {
+      targetLength = targetLength-this.length;
+      if (targetLength > padString.length) {
+        padString += padString.repeat(targetLength/padString.length);
+      }
+      return padString.slice(0,targetLength) + String(this);
+    }
+  };
+}
+
+function getHeatTime() {
+  const target = document.getElementById(TIME_FIELD_ID);
+  const start = Number(target.value) * 1000;
+  const period = Date.now() - start;
+
+  const min = Math.floor(period / (1000 * 60));
+  let sec = Math.floor(period % (1000 * 60) / 1000);
+  if (sec === 60) {
+    sec = 0;
+  }
+  return `${min.toFixed(0).padStart(2, '0')}:${sec.toFixed(0).padStart(2, '0')}`;
+}
+
+function showHeatTime() {
+  const target = document.getElementById(TIME_OUTPUT);
+  target.textContent = getHeatTime();
+}
+
+function getTargetUrl() {
+  const target = document.getElementById(CONTROL_FIELD_ID);
+  return target.dataset.url;
+}
+
+function getCheckPeriod() {
+  const target = document.getElementById(CONTROL_FIELD_ID);
+  return (Number(target.dataset.period) || 5) * 1000;
+}
 
 function getCurrentData() {
   const target = document.getElementById(CONTROL_FIELD_ID);
@@ -14,19 +60,25 @@ function compare(newData) {
   return newData.trim() !== currentData.trim();
 }
 
+function notify(callback) {
+  const player = document.createElement('audio');
+  player.src = NOTIFICATION_SOUND;
+  document.body.appendChild(player);
+  player.addEventListener('ended', callback);
+  player.play();
+}
+
 function checkUpdates(check, callback) {
   const xhr = new XMLHttpRequest();
-  xhr.timeout = CHECK_PERIOD - 500;
+  xhr.timeout = getCheckPeriod() - 500;
   xhr.addEventListener('load', e => {
-    console.log('Проверка');
     if (xhr.status !== 200) {
-      setTimeout(checkUpdates, CHECK_PERIOD);
-      return;
+      return next();
     }
     const actualData = xhr.response;
     if (check(actualData)) {
       console.log('Перезагрузка');
-      return callback();
+      return notify(callback);
     }
     next();
   });
@@ -36,14 +88,19 @@ function checkUpdates(check, callback) {
   xhr.addEventListener('timeout', e => {
     next();
   });
-  xhr.open('GET', TARGET_URL);
+  xhr.open('GET', getTargetUrl());
   xhr.send();
 }
 
 function next() {
   setTimeout(() => {
     checkUpdates(compare, () => location.reload());
-  }, CHECK_PERIOD);
+  }, getCheckPeriod());
 }
 
-next();
+function init() {
+  next();
+  setInterval(showHeatTime, 1000);
+}
+
+init();
